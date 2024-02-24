5cd96a981e4cc18c313c91d763bd9c267f99ef12
Fernando da Costa Felício <99772921+ffelicio82@users.noreply.github.com>
2024-02-24 16:16:25 -0300

    feat: estudando os tipos de variáveis (#3)
    Co-authored-by: Fernando da Costa Felício <ffeliciodeveloper@gmail.com>

---
+++ modulo-01/aula-03/TiposDeVariaveis/src/main/kotlin/TiposDeVariaveis.kt
@@ -0,0 +1,17 @@
+/*
+ * String = cadeia de caracteres, textos livres
+ * Int = armazena números inteiros, exemplo: 1, 2, 3, 4, ...
+ * Double = armazena números com pontos flutuantes, exemplo: 9.3, 0.5
+ * Boolean = true ou false
+ */
+
+fun main() {
+    val nomeSobrenome = "Fernando Felício"
+    val idade = 41
+    val altura = 1.67
+    val casado = true
+    println("nome e sobrenome: $nomeSobrenome")
+    println("idade: $idade")
+    println("altura: $altura")
+    println("casado: $casado")
+}
